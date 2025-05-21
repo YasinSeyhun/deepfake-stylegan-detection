91c47982c8565713683389f586121fbe2840ce63
YasinSeyhun <seyhunyasin@gmail.com>
2025-05-22 00:13:31 +0300

    .venv commit

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\MSI\Desktop\deepfake-stylegan-detection\.venv
+set VIRTUAL_ENV=C:\Users\MSI\Desktop\deepfake-stylegan-detection\backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
